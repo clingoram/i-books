0e946a58bc16267006df976cde8bea28547ababe
clingoram <llingchen.27@gmail.com>
2016-10-27 11:06:30 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function() { 
+
+    function bookSearch() {
+        console.log("It's working!!");
+
+        var keyword = document.getElementById('search').value;
+        document.getElementById('result').innerHTML = "";
+        console.log("You're looking for" +" "+ keyword);
+        
+       $.ajax({
+           url: 'https://www.googleapis.com/books/v1/volumes?q=' + keyword, //google books
+            dataType:'json',
+            type: 'GET',
+            success: function (data) {
+                for (i = 0; i < data.items.length; i++) {
+                    result.innerHTML += "<div class='title'>" + data.items[i].volumeInfo.title + "</div>" + "Authors:" + "<div class='author'>" + data.items[i].volumeInfo.authors + "</div>" + "Publisher:" + "<div class='publisher'>" + data.items[i].volumeInfo.publisher + "<div> " + "<div class='cover'>" + '<img src="' + data.items[i].volumeInfo.imageLinks.thumbnail + '"/>' + "</div>"+ "<div class='desrc'>" + data.items[i].volumeInfo.description + "</div>";
+                    },
+                    fail: function (data) {
+                        console.log("Oops!Can't find data....")
+                    }
+                })
+    };
+    document.getElementById('button').addEventListener('click', bookSearch, false);
+});
